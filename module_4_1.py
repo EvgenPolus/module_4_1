32cad2e115f45ab64fd0a66863cc7bf067ded1d8
EvgenPolus <teh.polus@gmail.com>
2024-11-02 00:40:20 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,18 @@
+# module_4_1.ru
+
+# from true_math import *
+# from fake_math import *
+import true_math as true
+import fake_math as fake
+
+true.true_divide()
+fake.fake_divide()
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
